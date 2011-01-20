d0b8f0d9f12c4cc57e363c461765bb8dfaff9fc9
David Pollum <david.pollum@intersect.org.au>
2011-01-20 15:57:37 +1100

    Partial user model with passwords, and user show page

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,8 +2,25 @@ require 'spec_helper'
 
 describe UsersController do
   render_views
+    
+   describe "Get 'show'" do
+     
+     before(:each) do
+      @user = Factory(:user)
+     end
+    
+    it "should be successful" do
+      get :show, :id => @user
+      response.should be_success
+    end
 
-  describe "GET 'new'" do
+    it "should find the right user" do
+     get :show, :id => @user
+     assigns(:user).should == @user
+    end
+   end
+  
+   describe "GET 'new'" do
     it "should be successful" do
       get 'new'
       response.should be_success

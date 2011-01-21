1d1c6718f9cad69d7daa2bbcc20956da6ea28798
David Pollum <david.pollum@intersect.org.au>
2011-01-21 13:56:47 +1100

    Finished CH9

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -45,6 +45,13 @@ require 'rspec/rails'
    def test_sign_in(user)
      controller.sign_in(user)
    end
+
+    def integration_sign_in(user)
+      visit signin_path
+      fill_in :email, 	:with => user.email
+      fill_in :password, :with => user.password
+      click_button
+    end
   end
 end
 
